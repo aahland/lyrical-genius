68661eddbfea8c91492c0137f885bab5eac1c27d
aahland <ahlandera@hotmail.com>
2022-03-17 14:48:49 +0100

    chore: initial setup

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	
+</script>
+
+<svelte:head>
+	<title>Lyrical Genius</title>
+</svelte:head>
+
+
+
+<style>
+	
+</style>

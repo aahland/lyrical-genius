d135f475e9cd681110045124e2982e839e5b3a86
aahland <ahlandera@hotmail.com>
2022-04-07 15:41:27 +0200

    fix: changes name on component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,7 +4,7 @@
 	import Round2 from '../lib/Round2.svelte';
 	import Round3 from '../lib/Round3.svelte';
 	import Round4 from '../lib/Round4.svelte';
-	import GameFinished from '../lib/GameFinished.svelte';
+	import GameFinished from '../lib/Gamefinished.svelte';
 	import Footer from '../lib/Footer.svelte';
 	import Signin from '../lib/Signin.svelte';
 

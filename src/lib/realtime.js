945a8ac741a5d2013dbc22867253d4566a9b1b02
aahland <ahlandera@hotmail.com>
2022-04-08 10:51:00 +0200

    fix: changing enpoint again

--- src/lib/realtime.js
+++ src/lib/realtime.js
@@ -1,5 +1,5 @@
 import ioClient from "socket.io-client";
-const ENDPOINT = "50f31430.lyrical-genius.pages.dev";
+const ENDPOINT = "https://50f31430.lyrical-genius.pages.dev";
 
 const socket = ioClient(ENDPOINT)
 

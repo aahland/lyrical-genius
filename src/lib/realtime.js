f23bdc2d508f02f8e05abb1c7158700a2f013db8
aahland <ahlandera@hotmail.com>
2022-04-08 10:47:03 +0200

    fix: changes local host to deployed endpoint for socket

--- src/lib/realtime.js
+++ src/lib/realtime.js
@@ -1,5 +1,5 @@
 import ioClient from "socket.io-client";
-const ENDPOINT = "http://localhost:3000";
+const ENDPOINT = "https://50f31430.lyrical-genius.pages.dev/";
 
 const socket = ioClient(ENDPOINT)
 

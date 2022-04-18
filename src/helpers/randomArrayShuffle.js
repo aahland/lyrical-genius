d6a6e7c6a6bca7446e530abda98ab7cc0207f36d
aahland <ahlandera@hotmail.com>
2022-04-18 08:55:04 +0200

    feat: shufflearray as its own function

---
+++ src/helpers/randomArrayShuffle.js
@@ -0,0 +1,17 @@
+//code copied from stack overflow, think it was this https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+export function randomArrayShuffle(array) {
+    var currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+

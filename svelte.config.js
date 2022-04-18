82817d3128b6dd7b54e9e8ff570fe6aafe306c5a
aahland <ahlandera@hotmail.com>
2022-04-18 09:21:59 +0200

    fix: changes to setup

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { Server } from 'socket.io';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: node({ env: { port: process.env.PORT } }),
+		adapter: node(),
 		vite: {
 			plugins: [
 				{

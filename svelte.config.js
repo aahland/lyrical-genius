9554c5c3c818bff8668bae39463d78e396342a9f
aahland <ahlandera@hotmail.com>
2022-04-20 18:15:04 +0200

    changes to adapter in config for heroku deploy

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ import node from '@sveltejs/adapter-node'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: node({ env: { port: process.env.PORT } }),
+		adapter: node({ out: 'production' }),
 		vite: {
 			plugins: [
 				{

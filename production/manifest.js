83ed7732b7a3b73553c9c63c996820b5fd2b8bf0
aahland <ahlandera@hotmail.com>
2022-04-20 18:22:52 +0200

    same as before

--- production/manifest.js
+++ production/manifest.js
@@ -3,7 +3,7 @@ export const manifest = {
 	assets: new Set(["images/logoMobile.png","robots.txt","sounds/correct.wav","sounds/wrong.wav"]),
 	mimeTypes: {".png":"image/png",".txt":"text/plain",".wav":"audio/wav"},
 	_: {
-		entry: {"file":"start-be8b959f.js","js":["start-be8b959f.js","chunks/vendor-88422b5e.js"],"css":[]},
+		entry: {"file":"start-175d449b.js","js":["start-175d449b.js","chunks/vendor-88422b5e.js"],"css":[]},
 		nodes: [
 			() => import('./server/nodes/0.js'),
 			() => import('./server/nodes/1.js')

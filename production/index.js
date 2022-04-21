a680680d7bae9b4d488399dd55660c1214c1f61d
aahland <ahlandera@hotmail.com>
2022-04-21 13:14:12 +0200

    modifies static index.js

--- production/index.js
+++ production/index.js
@@ -25,6 +25,9 @@ var compression$2 = {exports: {}};
 var negotiator = {exports: {}};
 
 var charset = {exports: {}};
+var port = process.env.PORT || 5000;
+
+http.listen(port, () => console.log(`Listening on port ${port}`));
 
 /**
  * negotiator
